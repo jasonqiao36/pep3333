6effcdf196fa6434fc9cb5368879b7590b1685d0
jasonqiao36 <jasonqiao36@gmail.com>
2018-02-21 15:06:40 +0800

    wsgi server_side and app_side

---
+++ app_side.py
@@ -0,0 +1,25 @@
+HELLO_WORLD = b'Hello World! by jason\n'
+
+
+def simple_app(environ, start_response):
+	"""
+	扮演应用端角色，类似Flask。
+	提供状态码和头部信息，返回响应字符串。
+	服务端收到客户端HTTP请求后，将调用simple_app。
+	"""
+	status = '200 OK'
+	resp_headers = [('Content-Type', 'text/plain')]
+	start_response(status, resp_headers)
+	return [HELLO_WORLD]
+
+
+class AppClass:
+	def __init__(self, environ, start_response):
+		self.environ = environ
+		self.start_response = start_response
+
+	def __iter__(self):
+		status = '200 OK'
+		resp_headers = [('Content-Type', 'text/plain')]
+		self.start(status, resp_headers)
+		yield HELLO_WORLD
---
+++ server_side.py
@@ -0,0 +1,87 @@
+import os, sys
+
+# surrogateescape 是一种可逆的错误处理机制，利用 Surrogate 码位保存无法解码的字节，编码时则将其还原为对应的原始字节。
+enc, esc = sys.getfilesystemencoding(), 'surrogateescape'
+
+def unicode_to_wsgi(u):
+	# Convert an environment variable to a WSGI "bytes-as-unicode" string
+	# iso-8859-1就是latin-1
+	return u.encode(enc, esc).decode('iso-8859-1')
+
+def wsgi_to_bytes(s):
+	# 处理头部信息，转化为native string
+	return s.encode('iso-8859-1')
+
+
+def run_with_cgi(application):
+	"""
+	服务端收到HTTP请求后，调用application（这个例子中，就是run_simple)。
+	application调用start_response方法，设置headers_set
+	write(data)调用write方法，设置头部信息headers_sent，写入响应body
+	"""
+
+	# os.environ 	获取操作系统定义的环境变量
+	environ = {k:unicode_to_wsgi(v) for k,v in os.environ.items()}
+	environ['wsgi.input'] = sys.stdin.buffer	# sys.stdin 用于交互式输入
+	environ['wsgi.errors'] = sys.stderr			# sys.stderr 用于错误信息
+	environ['wsgi.version'] = (1, 0)
+	environ['wsgi.multithread'] = False
+	environ['wsgi.multiprocess'] = True
+	environ['wsgi.run_once'] = True
+
+	if environ.get('HTTPS', 'off') in ('on', '1'):
+		environ['wsgi.url_scheme'] = 'https'
+	else:
+		environ['wsgi.url_scheme'] = 'http'
+
+	headers_set = []
+	headers_sent = []
+
+	def write(data):
+		out = sys.stdout.buffer
+
+		if not headers_set:
+			raise AssertionError('write() before start_response()')
+
+		elif not headers_sent:
+			status, response_headers = headers_sent[:] = headers_set
+			out.write(wsgi_to_bytes('Status: %s\r\n' % status))
+			for header in response_headers:
+				out.write(wsgi_to_bytes('%s: %s\r\n' % header))
+			out.write(wsgi_to_bytes('\r\n'))
+
+		out.write(data)
+		out.flush()
+
+	def start_response(status, response_headers, exc_info=None):
+		if exc_info:
+			try:
+				if headers_sent:
+					raise exc_info[1].with_traceback(exc_info[2])
+			finally:
+				exc_info = None
+		elif headers_set:
+			raise AssertionError('Headers already set!')
+
+		headers_set[:] = [status, response_headers]
+
+		# Note: error checking on the headers should happen here
+
+		return write
+
+	result = application(environ, start_response)
+
+	try:
+		for data in result:
+			if data:
+				write(data)		# don't send headers until body appears
+		if not headers_sent:
+			write('')		# send headers now if body was empty
+	finally:
+		if hasattr(result, 'close'):
+			result.close()
+
+if __name__ == '__main__':
+	from app_side import simple_app
+
+	run_with_cgi(simple_app)
