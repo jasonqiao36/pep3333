4510156e143ea1896e09454103a8b7a4871bdeeb
JasonQiao36 <jasonqiao36@gmail.com>
2018-02-23 14:00:04 +0000

    fix pep8

--- socket_server.py
+++ socket_server.py
@@ -1,10 +1,14 @@
+"""
+complete a web server by socket
+python2
+"""
 # coding=utf8
 
 import socket
 
 EOL1 = '\n\n'
 EOL2 = '\n\r\n'
-body = """Hello World! <h1>from the5fire 《Django企业开发实战》</h1>"""
+body = """Hello World! <h1>你好 Jason</h1>"""
 resp_params = [
     'HTTP/1.0 200 OK',
     'Date: Sat, 10 jun 2017 01:01:01 GMT',
@@ -30,10 +34,10 @@ def main():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 设置端口可复用，保证我们每次Ctrl C之后，快速再次重启
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversocket.bind(('127.0.0.1', 8080))
+    serversocket.bind(('0.0.0.0', 8080))
     # 设置连接数
     serversocket.listen(1)
-    print('http://127.0.0.1:8080')
+    print('http://0.0.0.0:8080')
 
     try:
         while True:
